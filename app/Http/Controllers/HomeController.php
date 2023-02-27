e03500c3da67b28a11a429e3aba2c73024807f3e
rayhanalshorif133 <rayhanalshorif4@gmail.com>
2023-02-27 14:16:50 +0600

    Notification system

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Larafirebase;
+use App\Notifications\SendPushNotification;
+use Illuminate\Support\Facades\Notification;
+
+class HomeController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function index()
+    {
+
+        return view('home');
+    }
+
+    public function saveToken(Request $request)
+    {
+        $user = User::find(auth()->user()->id);
+        $user->device_token = $request->token;
+        $user->save();
+        return response()->json(['success' => 'Token saved successfully.']);
+    }
+
+
+    public function sendNotification(Request $request)
+    {
+        $firebaseToken = User::whereNull('device_token')->pluck('device_token')->all();
+
+        $SERVER_API_KEY = env('FIREBASE_SERVER_KEY');
+
+        $data = [
+            "registration_ids" => $firebaseToken,
+            "notification" => [
+                "title" => $request->title,
+                "body" => $request->body,
+            ]
+        ];
+        $dataString = json_encode($data);
+
+        $headers = [
+            'Authorization: key=' . $SERVER_API_KEY,
+            'Content-Type: application/json',
+        ];
+
+        $ch = curl_init();
+
+        curl_setopt($ch, CURLOPT_URL, 'https://fcm.googleapis.com/fcm/send');
+        curl_setopt($ch, CURLOPT_POST, true);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $dataString);
+
+        $response = curl_exec($ch);
+
+        return response()->json(['success' => 'Notification sent successfully.']);
+    }
+}
---
+++ public/firebase-messaging-sw.js
@@ -0,0 +1,42 @@
+/*
+Give the service worker access to Firebase Messaging.
+Note that you can only use Firebase Messaging here, other Firebase libraries are not available in the service worker.
+*/
+importScripts('https://www.gstatic.com/firebasejs/7.23.0/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/7.23.0/firebase-messaging.js');
+
+/*
+Initialize the Firebase app in the service worker by passing in the messagingSenderId.
+* New configuration for app@pulseservice.com
+*/
+firebase.initializeApp({
+        apiKey: "AIzaSyDurnWw_YcrZGhHqW8YYVoaFATeNYQHfik",
+        authDomain: "push-notification-29dac.firebaseapp.com",
+        projectId: "push-notification-29dac",
+        storageBucket: "push-notification-29dac.appspot.com",
+        messagingSenderId: "572029365357",
+        appId: "1:572029365357:web:0c60da1c06aabe296ee364",
+        measurementId: "G-B2ND36RYL6"
+    });
+
+/*
+Retrieve an instance of Firebase Messaging so that it can handle background messages.
+*/
+const messaging = firebase.messaging();
+messaging.setBackgroundMessageHandler(function(payload) {
+    console.log(
+        "[firebase-messaging-sw.js] Received background message ",
+        payload,
+    );
+    /* Customize notification here */
+    const notificationTitle = "Background Message Title";
+    const notificationOptions = {
+        body: "Background Message body.",
+        icon: "/itwonders-web-logo.png",
+    };
+
+    return self.registration.showNotification(
+        notificationTitle,
+        notificationOptions,
+    );
+});
